404ee0f77ee4a3eaed6269a17aef0d7f86b2bf4f
briannqc <brian.ngo313@gmail.com>
2022-05-15 17:14:40 +0800

    Init project: Install locust and run mock server with json-server

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo 'Installing locust in a venv'
+python3 -m venv .env
+source .env/bin/activate
+python3 -m pip install --upgrade pip
+pip3 install locust
+source .env/bin/activate
+locust -V
+
+echo 'Starting node server'
+npm install
+curl http://jsonplaceholder.typicode.com/db >db.json
+./node_modules/.bin/json-server --watch db.json
